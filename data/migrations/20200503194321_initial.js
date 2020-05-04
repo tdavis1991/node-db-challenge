d541c9468349426b3efa12007f93ff18de0a8e4c
tevin davis <aod.td1105@gmail.com>
2020-05-03 20:47:15 -0400

    Built out DB for project

--- data/migrations/20200503194321_initial.js
+++ data/migrations/20200503194321_initial.js
@@ -1,10 +1,57 @@
 
 exports.up = async function(knex) {
   await knex.schema.createTable('projects', (table) => {
-      
+      table.increments('id')
+      table.text('name').notNull()
+      table.text('description')
+      table.boolean('completed').default(false)
   })
+
+  await knex.schema.createTable('tasks', (table) => {
+      table.increments('id')
+      table.text('description').notNull()
+      table.text('notes')
+      table.boolean('completed').default(false)
+  })
+
+  await knex.schema.createTable('resources', (table) => {
+      table.increments('id')
+      table.text('name').notNull()
+      table.text('description')
+  })
+
+  await knex.schema.createTable('project_task', (table) => {
+      table.integer('project_id')
+            .references('id')
+            .inTable('projects')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+      table.integer('task_id')
+            .references('id')
+            .inTable('task')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+  })
+
+  await knex.schema.createTable('project_resource', (table) => {
+    table.integer('project_id')
+          .references('id')
+          .inTable('projects')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+    table.integer('resource_id')
+          .references('id')
+          .inTable('resources')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+    })
 };
 
-exports.down = function(knex) {
-  
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('project_task')
+    await knex.schema.dropTableIfExists('project_resource')
+    await knex.schema.dropTableIfExists('resources')
+    await knex.schema.dropTableIfExists('tasks')
+    await knex.schema.dropTableIfExists('projects')
 };
